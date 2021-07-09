586abffb5ed2145453054d4cf6f8bc7839d95d69
ashBHF <84406313+ashBHF@users.noreply.github.com>
2021-07-08 22:20:36 -0600

    cleaning JS

--- source/script.js
+++ source/script.js
@@ -50,44 +50,29 @@ displayFarenheit.addEventListener("click", outputFarenheit);
 */
 
 //https://api.openweathermap.org/data/2.5/weather?q=denver&appid=c284e41e5087d96e9a0af3b148134460
-function search(city) {
-  let apiKey = "c284e41e5087d96e9a0af3b148134460";
-  let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-  axios.get(`${apiUrl}`).then(populateCircleOne);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-input");
-  let city = searchInput.value;
-  search(city);
-}
 
-let citySearch = document.querySelector("#city-search");
-citySearch.addEventListener("submit", handleSubmit);
 
-function currentPositionWeather(position) {
-  let units = "imperial";
+/*function currentPositionWeather(position) {
+  let units = "metric";
   let apiKey = "c284e41e5087d96e9a0af3b148134460";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(populateCircleOne);
 }
 
 navigator.geolocation.getCurrentPosition(currentPositionWeather);
-
-let button = document.querySelector("button");
-button.addEventListener("click", currentPositionWeather);
+*/
 
 function populateCircleOne(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#number");
-  currentTemperature.innerHTML = `${temperature}°F`;
-
+  
   let city = response.data.name;
   let cityHeader = document.querySelector(".city");
   cityHeader.innerHTML = `${city}`;
 
-  let weather = response.data.weather[0].main;
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#number");
+  currentTemperature.innerHTML = `${temperature}°`;
+
+  let weather = response.data.weather[0].description;
   let weatherHeader = document.querySelector(".weather");
   weatherHeader.innerHTML = `${weather}`;
 
@@ -103,14 +88,46 @@ function populateCircleOne(response) {
   let weatherIcon = document.querySelector("#weather-icon");
   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${pngIdentifier}@2x.png`); 
 
-  let windSummary = response.data.wind.speed;
+  let windSummary = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector(".wind-speed");
-  windSpeed.innerHTML = `wind: ${windSummary} meters/sec`;
+  windSpeed.innerHTML = (`wind: ${windSummary} km/hr`);
 
   let humiditySummary = response.data.main.humidity;
   let humidityElement = document.querySelector(".humidity");
   humidityElement.innerHTML = `humidity: ${humiditySummary}%`;
-
-
 }
 
+function search(city) {
+  let apiKey = "c284e41e5087d96e9a0af3b148134460";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(`${apiUrl}`).then(populateCircleOne);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+  let city = searchInput.value;
+  search(city);
+}
+
+let citySearch = document.querySelector("#city-search");
+citySearch.addEventListener("submit", handleSubmit);
+
+/*function displayFarenheitTemperature(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#number");
+  //let farenheitEquation = (`${temperature}` * 9) / 5 + 32;
+  currentTemperature.innerHTML = (`${temperature}` * 9) / 5 + 32;
+}
+
+  //let temperature = Math.round(response.data.main.temp);
+  //let currentTemperature = document.querySelector("#number");
+  //currentTemperature.innerHTML = `${temperature}°`;
+
+//let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#farenheit-conversion");
+fahrenheitLink.addEventListener("click", displayFarenheitTemperature);
+*/
+search("fort collins");
