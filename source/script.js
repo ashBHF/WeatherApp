9695b58bc1c1346ee22ee5dc4fb4ebcf30b42761
ashBHF <84406313+ashBHF@users.noreply.github.com>
2021-07-09 13:03:32 -0600

    added celsius <-> farenheit conversion JS

--- source/script.js
+++ source/script.js
@@ -15,39 +15,7 @@ let day = days[today.getDay()];
 let time = today.toLocaleTimeString([], { timeStyle: "short" });
 
 getDate.innerHTML = `${day}, ${time}`;
-/*
-function cityInput(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-input");
-  //alert(searchInput.value);
 
- let divCity = document.querySelector(".city");
-  if (searchInput.value) {
-    divCity.innerHTML = `${searchInput.value}`;
-  }}
-
-
-let enterCity = document.querySelector("#city-search");
-enterCity.addEventListener("submit", cityInput);
-
-function outputCelsius(event) {
-  event.preventDefault();
-  let changeCelsius = document.querySelector("#number");
-  changeCelsius.innerHTML = "16°";
-}
-
-let displayCelsius = document.querySelector("#celsius");
-displayCelsius.addEventListener("click", outputCelsius);
-
-function outputFarenheit(event) {
-  event.preventDefault();
-  let changeFarenheit = document.querySelector("#number");
-  changeFarenheit.innerHTML = "60.8°";
-}
-
-let displayFarenheit = document.querySelector("#farenheit");
-displayFarenheit.addEventListener("click", outputFarenheit);
-*/
 
 //https://api.openweathermap.org/data/2.5/weather?q=denver&appid=c284e41e5087d96e9a0af3b148134460
 
@@ -63,6 +31,8 @@ navigator.geolocation.getCurrentPosition(currentPositionWeather);
 */
 
 function populateCircleOne(response) {
+
+  celsiusTemperature = response.data.main.temp;
   
   let city = response.data.name;
   let cityHeader = document.querySelector(".city");
@@ -114,20 +84,27 @@ function handleSubmit(event) {
 let citySearch = document.querySelector("#city-search");
 citySearch.addEventListener("submit", handleSubmit);
 
-/*function displayFarenheitTemperature(event) {
+function displayFarenheitTemperature(event) {
   event.preventDefault();
   let currentTemperature = document.querySelector("#number");
-  //let farenheitEquation = (`${temperature}` * 9) / 5 + 32;
-  currentTemperature.innerHTML = (`${temperature}` * 9) / 5 + 32;
+  let farenheitEquation = (celsiusTemperature * 9) / 5 + 32;
+  currentTemperature.innerHTML = `${Math.round(farenheitEquation)}°`;
 }
 
-  //let temperature = Math.round(response.data.main.temp);
-  //let currentTemperature = document.querySelector("#number");
-  //currentTemperature.innerHTML = `${temperature}°`;
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#number");
+  currentTemperature.innerHTML = `${Math.round(celsiusTemperature)}°`;
 
-//let celsiusTemperature = null;
+}
+
+let celsiusTemperature = null;
 
 let fahrenheitLink = document.querySelector("#farenheit-conversion");
 fahrenheitLink.addEventListener("click", displayFarenheitTemperature);
-*/
+
+let celsiusLink = document.querySelector("#celsius-conversion");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
 search("fort collins");
